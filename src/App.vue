44db3360b871fa0939bfc82f24b0a11d84e514bb
Aleksandr <zheltkevich42@gmail.com>
2021-03-14 13:19:29 +0300

    Add default global styles

--- src/App.vue
+++ src/App.vue
@@ -20,26 +20,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
